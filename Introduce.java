2b4888bfee8f637fcde3d2431856fcc982c8eec5
maeda <maesann@outlook.jp>
2023-06-22 07:32:45 +0900

    ソース追加

---
+++ Introduce.java
@@ -0,0 +1,18 @@
+class Introduce{
+  public static void main(String[] args){
+  	System.out.println("V“üĞˆõ‚Ì©ŒÈĞ‰î‚ğ‚µ‚Ü‚·B");
+  	
+  	System.out.println("");
+  	
+  	System.out.println("");
+  	
+  	System.out.println("");
+  	
+  	System.out.println("");
+  	
+  	System.out.println("");
+  	
+  	System.out.println("");
+  	
+  }
+}
